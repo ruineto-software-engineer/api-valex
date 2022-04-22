1683b00c9b8f3b3ae4504ea74447d52c2ab54ee1
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-04-22 11:06:23 -0300

    Preparando revisão de código

--- src/services/rechargeService.ts
+++
@@ -1,5 +0,0 @@
-import * as rechargeRepository from '../repositories/rechargeRepository.js';
-
-export async function insertRecharge(rechargeData) {
-	await rechargeRepository.insert(rechargeData);
-}
--- src/utils/sqlUtils.ts
+++
@@ -1,8 +0,0 @@
-export function mapObjectToUpdateQuery({ object, offset = 1 }) {
-  const objectColumns = Object.keys(object)
-    .map((key, index) => `"${key}"=$${index + offset}`)
-    .join(",");
-  const objectValues = Object.values(object);
-
-  return { objectColumns, objectValues };
-}
